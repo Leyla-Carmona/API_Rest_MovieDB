2ab7625b6bf7bfd6bb74a0c88afa34eb0d924b46
Leyla Carmona <leylacarmona2010@hotmail.es>
2025-02-12 03:47:03 -0600

    $git commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("contraseña es:"+ key);
+async function mode(){
+    let body = document.body.className;
+    if(body == "darkmode")
+    {
+        document.body.className ="lightmode";
+    } 
+    else{
+        document.body.className ="darkmode";
+    }
+}
+
+const menu = document.getElementById("menu");
+const btn = document.getElementById("menu-btn");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("active"); // Alterna la clase "hidden"
+});
